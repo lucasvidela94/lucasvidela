5b9f805ff119e719c7032e967b97ebee59e8dd88
lucasvidela94 <lucas.videla994@gmail.com>
2022-06-23 14:55:51 -0300

    Exported new component

--- components/index.js
+++ components/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from "./Navigation/Header.nav";
 export { default as About } from "./Sections/About.section";
+export { default as Projects } from "./Sections/Projects.section";
