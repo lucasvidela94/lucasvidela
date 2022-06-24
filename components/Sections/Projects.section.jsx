6543c0dfd93619fbe4888e3c79055bba254ef2c1
lucasvidela94 <lucas.videla994@gmail.com>
2022-06-24 16:49:37 -0300

    Fix responsiveness for cards container

--- components/Sections/Projects.section.jsx
+++ components/Sections/Projects.section.jsx
@@ -3,7 +3,7 @@ import Card from "../Elements/Card";
 
 const Projects = () => {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-wrap justify-center -mx-1 lg:-mx-4">
       <Card
         texto="Info del proyecto1"
         imagen="https://i.pinimg.com/564x/19/e6/d3/19e6d346aecc9d2a7f69f2ab90424c9a.jpg"
