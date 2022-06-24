eeef04a856e441cea5a23eef72bd259dd7c7c797
lucasvidela94 <lucas.videla994@gmail.com>
2022-06-24 16:36:30 -0300

    Modified navbar style

--- components/Navigation/Header.nav.jsx
+++ components/Navigation/Header.nav.jsx
@@ -1,11 +1,11 @@
-import React from "react";
 import Link from "next/dist/client/link";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import Image from "next/image";
 
 const TextBox = ({ text, url }) => {
   return (
     <a
-      className="block text-white rounded md:bg-transparent md:text-white-700 md:p-0 dark:text-white hover:opacity-10"
+      className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-gray-200 duration-100 hover:bg-zinc-800"
       href={url}
     >
       {text}
@@ -15,19 +15,31 @@ const TextBox = ({ text, url }) => {
 
 const Header = () => {
   return (
-    <nav className="bg-white border-gray-400 px-2 py-2.5 dark:bg-gray-800">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <TextBox text="Home" url="#home" />
-        <TextBox text="Acerca de mi " url="#aboutme" />
-        <TextBox text="Proyectos" url="#projects" />
-        <TextBox text="Contacto" url="#contact" />
-        <Link href="https://www.google.com">
-          <a target="_blank">
-            <GitHubIcon />
-          </a>
-        </Link>
-      </div>
-    </nav>
+    <div>
+      <nav className=" border-gray-400 px-2 py-2.5 ">
+        <div className="hidden container lg:flex flex-wrap justify-between items-center mx-auto">
+          <TextBox text="Home" url="#home" />
+          <TextBox text="Acerca de mi " url="#aboutme" />
+          <TextBox text="Proyectos" url="#projects" />
+          <TextBox text="Contacto" url="#contact" />
+          <Link href="https://www.google.com">
+            <a target="_blank">
+              <GitHubIcon className="fill-white" />
+            </a>
+          </Link>
+        </div>
+        <div className="flex justify-between lg:hidden">
+          <Image
+            src="/svg/WolfIcon.svg"
+            alt="Logo"
+            width={25}
+            height={25}
+            className="invert"
+          />
+          <GitHubIcon className="fill-white" />
+        </div>
+      </nav>
+    </div>
   );
 };
 
