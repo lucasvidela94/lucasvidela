cf593169cf0ea11b6f55cddb22aeef234c3ee3f0
lucasvidela94 <lucas.videla994@gmail.com>
2022-07-11 00:01:50 -0300

    Create button to reach to the top and exported as a a module
    author: lucasvidela94 <lucas.videla994@gmail.com>

--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as About } from "./Sections/About.section";
 export { default as Projects } from "./Sections/Projects.section";
 export { default as Footer } from "./Sections/Footer.section";
 export { default as Skills } from "./Sections/Skills.section";
+export { default as ButtonToTop } from "./Elements/Button";
