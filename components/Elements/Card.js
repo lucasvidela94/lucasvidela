7c8d199eef503ef703f3daf6a95c3b9beeed38df
lucasvidela94 <lucas.videla994@gmail.com>
2022-07-24 06:00:04 -0300

    --amend

--- components/Elements/Card.js
+++ components/Elements/Card.js
@@ -2,10 +2,10 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkIcon from '@mui/icons-material/Link';
 
-const Card = ({ texto, imagen,github,deploy }) => {
+const Card = ({ texto, imagen,github,deploy,alt }) => {
   return (
     <div className="max-w-sm mx-8 my-8 overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl">
-      <img src={imagen} alt="project" className="h-auto w-full" />
+      <img src={imagen} alt="project" className="h-auto w-full" alt={alt} />
       <div className="p-5">
         <p className="text-medium font-semibold mb-5 text-gray-700">{texto}</p>
         <div className="flex flex-row justify-between">
--- components/Sections/Projects.section.jsx
+++ components/Sections/Projects.section.jsx
@@ -7,26 +7,30 @@ const Projects = () => {
       <Card
         texto="Amazon Spain Mockup created in React.js and deployed on firebase"
         imagen="https://media.giphy.com/media/OzsCCqkyMUpr68gSKa/giphy.gif"
-        github=""
-        deploy=""
+        github="https://github.com/lucasvidela94/react-amazon-store"
+        deploy="https://clon-de-20f60.web.app/"
+        alt="Amazon mockup"
       />
       <Card
         texto="Administrative tool for time management made on Ruby on Rails"
         imagen="https://media.giphy.com/media/czOpg0e3S2UoB6bArs/giphy.gif"
         github="https://github.com/BC-MAY-21-ROR/kata-06-reloj-checador-m22-krc-t1-reilero-02"
         deploy="https://hidden-castle-14008.herokuapp.com/"
+        alt="Check in clock"
       />
       <Card
         texto="Web application for small business to keep their budgets"
         imagen="https://media.giphy.com/media/7pjkNjEgl11sYfhsEX/giphy.gif"
-        github=""
+        github="https://github.com/BC-MAY-21-ROR/proyecto-final-SAPE-m22-pf-t2"
         deploy=""
+        alt="Sapec app"
       />
       <Card
         texto="Portfolio web made on NextJS and Tailwind"
         imagen="https://i.postimg.cc/N0FtL0XX/portfolio.png"
-        github=""
+        github="https://github.com/lucasvidela94/lucasvidela"
         deploy=""
+        alt="Portfolio"
       />
     </div>
   );
