d08e35b30d246c33c5b56a72e3b93d2d08322cbf
lucasvidela94 <lucas.videla994@gmail.com>
2022-08-08 01:33:54 -0300

    Fixed margin on button and added cv download

--- components/Sections/About.section.jsx
+++ components/Sections/About.section.jsx
@@ -65,7 +65,11 @@ const About = () => {
           >
             <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
           </svg>
-          <a href="https://www.google.com/" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/lucasvidela94/cv/raw/master/CURRICULUM-LUCAS%20VIDELA.pdf/"
+            target="_blank"
+            rel="noreferrer"
+          >
             CV!
           </a>
         </button>
